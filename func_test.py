d8892dba09bc1f2951568f28e1247ad527167913
I306454 <jiabin.zheng01@sap.com>
2020-01-20 18:04:48 +0800

    new readme of process status

---
+++ func_test.py
@@ -0,0 +1,15 @@
+import os, sys
+print('version info: %s' % (sys.version_info, ))
+print('file real path: %s' % os.path.realpath(__file__))
+print('file dir: %s' % os.path.dirname(os.path.realpath(__file__)))
+print('file name: %s' % os.path.dirname(sys.argv[0]))
+print(sys.argv, len(sys.argv))
+print(sys.argv[0])
+print(sys.argv[1])
+'''
+Console output:
+
+version info: sys.version_info(major=3, minor=7, micro=5, releaselevel='final', serial=0)
+file dir: /Users/i306454/gitStore/you-get/build_in_func_test.py
+file name: 
+'''
